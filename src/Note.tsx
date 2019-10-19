a65020212f461d83119ad68b46a5479b01edce5c
Cure Git <37978051+curegit@users.noreply.github.com>
2019-10-19 22:14:29 +0900

    Impl edit function

--- src/Note.tsx
+++ src/Note.tsx
@@ -2,10 +2,19 @@ import React from "react";
 
 type Props = { id: number, content: string, setNotes: SetNoteFunc };
 const Note: React.FunctionComponent<Props> = props => {
+  const textareaRef = React.useRef<HTMLTextAreaElement>(null);
+  const edit = React.useCallback(() => props.setNotes((prev) => prev.map(n => {
+    if (n.id == props.id) {
+      if (textareaRef.current != null) {
+        n.content = textareaRef.current.value
+      }
+    }
+    return n;
+  })), [props.setNotes, textareaRef.current])
   const del = React.useCallback(() => props.setNotes((prev) => prev.filter(n => n.id != props.id)), [props.setNotes])
   return (
     <div className="note">
-      <div className="content">{props.content}</div>
+      <textarea className="content" value={props.content} onChange={edit} ref={textareaRef}></textarea>
       <div className="delete-action">
         <span onClick={del}>Delete</span>
       </div>
