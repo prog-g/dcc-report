8f33c49bb88858ffa5203fc85fc81cc7c04f1c42
Cure Git <37978051+curegit@users.noreply.github.com>
2019-10-21 21:57:47 +0900

    Fix format

--- src/Note.tsx
+++ src/Note.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 
 type Props = { id: number, content: string, setNotes: SetNoteFunc };
+
 const Note: React.FunctionComponent<Props> = props => {
   const textareaRef = React.useRef<HTMLTextAreaElement>(null);
-  const edit = React.useCallback(() => props.setNotes((prev) => prev.map(n => {
+  const edit = React.useCallback(() => props.setNotes(prev => prev.map(n => {
     if (n.id == props.id) {
       if (textareaRef.current != null) {
-        n.content = textareaRef.current.value
+        n.content = textareaRef.current.value;
       }
     }
     return n;
-  })), [props.setNotes, textareaRef.current])
-  const del = React.useCallback(() => props.setNotes((prev) => prev.filter(n => n.id != props.id)), [props.setNotes])
+  })), [props.setNotes, textareaRef.current]);
+  const del = React.useCallback(() => props.setNotes(prev => prev.filter(n => n.id != props.id)), [props.setNotes]);
   return (
     <div className="note">
       <textarea className="content" value={props.content} onChange={edit} ref={textareaRef}></textarea>
--- src/Timeline.tsx
+++ src/Timeline.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Note from "./Note";
 
-type Props = { notes: { id: number; content: string }[], setNotes: SetNoteFunc }
+type Props = { notes: { id: number; content: string }[], setNotes: SetNoteFunc };
 
 const Timeline: React.FunctionComponent<Props> = props => {
-  const add = React.useCallback(() => props.setNotes((prev) => [...prev, { id: prev.reduce((i, j) => i.id < j.id ? j : i).id + 1, content: "" }]), [props.setNotes]);
+  const add = React.useCallback(() => props.setNotes(prev => [...prev, { id: prev.reduce((i, j) => i.id < j.id ? j : i).id + 1, content: "" }]), [props.setNotes]);
   const notes = props.notes.map(n => <Note key={n.id} id={n.id} content={n.content} setNotes={props.setNotes}/>);
   return (
     <div className="timeline">
