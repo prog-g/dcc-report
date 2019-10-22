7c51682b39515146f5e9d667e20e64ae94f0729c
Cure Git <37978051+curegit@users.noreply.github.com>
2019-10-22 14:57:09 +0900

    Fix reduce

--- src/Timeline.tsx
+++ src/Timeline.tsx
@@ -4,7 +4,7 @@ import Note from "./Note";
 type Props = { notes: { id: number; content: string }[], setNotes: SetNoteFunc };
 
 const Timeline: React.FunctionComponent<Props> = props => {
-  const add = React.useCallback(() => props.setNotes(prev => [...prev, { id: prev.reduce((i, j) => i.id < j.id ? j : i).id + 1, content: "" }]), [props.setNotes]);
+  const add = React.useCallback(() => props.setNotes(prev => [...prev, { id: prev.reduce((a, n) => a < n.id ? n.id : a, 0) + 1, content: "" }]), [props.setNotes]);
   const notes = props.notes.map(n => <Note key={n.id} id={n.id} content={n.content} setNotes={props.setNotes}/>);
   return (
     <div className="timeline">
