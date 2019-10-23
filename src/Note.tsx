5a828f47bf9c23a099ef35fdc992afdd8e80d293
Cure Git <37978051+curegit@users.noreply.github.com>
2019-10-23 11:30:00 +0900

    ref -> event

--- src/Note.tsx
+++ src/Note.tsx
@@ -3,19 +3,19 @@ import React from "react";
 type Props = { id: number, content: string, setNotes: SetNoteFunc };
 
 const Note: React.FunctionComponent<Props> = props => {
-  const textareaRef = React.useRef<HTMLTextAreaElement>(null);
-  const edit = React.useCallback(() => props.setNotes(prev => prev.map(n => {
-    if (n.id === props.id) {
-      if (textareaRef.current !== null) {
-        n.content = textareaRef.current.value;
+  const edit = React.useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    e.persist();
+    props.setNotes(prev => prev.map(n => {
+      if (n.id === props.id) {
+        n.content = e.target.value;
       }
-    }
-    return n;
-  })), [props.setNotes, textareaRef.current]);
+      return n;
+    }));
+  }, [props.setNotes]);
   const del = React.useCallback(() => props.setNotes(prev => prev.filter(n => n.id !== props.id)), [props.setNotes]);
   return (
     <div className="note">
-      <textarea className="content" value={props.content} onChange={edit} ref={textareaRef}></textarea>
+      <textarea className="content" value={props.content} onChange={edit}></textarea>
       <div className="delete-action">
         <span onClick={del}>Delete</span>
       </div>
