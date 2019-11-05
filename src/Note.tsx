b193a7969a3865f0a650dec12d1d400809e0f374
Cure Git <37978051+curegit@users.noreply.github.com>
2019-11-05 23:43:35 +0900

    Insert above

--- src/Note.tsx
+++ src/Note.tsx
@@ -4,6 +4,16 @@ type Props = { id: number; setNotes: SetNoteFunc };
 
 const Note: React.FunctionComponent<Props> = props => {
   const [content, setContent] = React.useState("new note");
+  const insert = React.useCallback(() => {
+    props.setNotes(prev => {
+      const i = prev.findIndex(n => n.id == props.id);
+      const maxId = prev.reduce((a, n) => (a < n.id ? n.id : a), 0);
+      return prev
+        .slice(0, i)
+        .concat([{ id: maxId + 1 }])
+        .concat(prev.slice(i, prev.length));
+    });
+  }, [props]);
   const edit = React.useCallback(
     (e: React.ChangeEvent<HTMLTextAreaElement>) => setContent(e.target.value),
     []
@@ -14,6 +24,9 @@ const Note: React.FunctionComponent<Props> = props => {
   );
   return (
     <div className="note">
+      <div className="insert-action">
+        <span onClick={insert}>+ Insert Above</span>
+      </div>
       <textarea className="content" value={content} onChange={edit}></textarea>
       <div className="delete-action">
         <span onClick={del}>Delete</span>
