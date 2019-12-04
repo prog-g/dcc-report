b9a9b857ef5cc95d95c05ab63a04386f5073773c
Cure Git <37978051+curegit@users.noreply.github.com>
2019-12-04 14:11:58 +0900

    曲線

--- src/declares.d.ts
+++ src/declares.d.ts
@@ -1,2 +1,24 @@
+type Point = { x: number; y: number };
+type SetPointsFunc = React.Dispatch<React.SetStateAction<Point[]>>;
 type NoteState = { id: number };
 type SetNotesFunc = React.Dispatch<React.SetStateAction<NoteState[]>>;
+type GraphFunc = (x: number) => number | null;
+type Curve = {
+  f: GraphFunc;
+  df: GraphFunc;
+  d2f: GraphFunc;
+  start: number;
+  end: number;
+  min: number;
+  max: number;
+};
+type Graph = {
+  f: GraphFunc;
+  df: GraphFunc;
+  d2f: GraphFunc;
+  start: number | null;
+  end: number | null;
+  min: number | null;
+  max: number | null;
+  points: Point[];
+};
