14e7468ae777f11aa2e9381ea276f507f22533df
Cure Git <37978051+curegit@users.noreply.github.com>
2019-12-08 00:47:48 +0900

    fix

--- src/lib.ts
+++ src/lib.ts
@@ -1,18 +1,18 @@
-function linearLineSeg(p1: Point, p2: Point): Curve {
+function linearLineSegment(p1: Point, p2: Point): Curve {
   const a = (p1.y - p2.y) / (p1.x - p2.x);
   const b = p1.y - a * p1.x;
   const f = (x: number): number => a * x + b;
-  const df = (x: number): number => 0 * x + a;
-  const d2f = (x: number): number => 0 * x;
-  const start = Math.min(p1.x, p2.x);
-  const end = Math.max(p1.x, p2.x);
-  const exts = [f(start), f(end)];
+  const df = (): number => a;
+  const d2f = (): number => 0;
+  const from = Math.min(p1.x, p2.x);
+  const to = Math.max(p1.x, p2.x);
+  const exts = [f(from), f(to)];
   const min = exts.reduce((a, y) => (a > y ? y : a));
   const max = exts.reduce((a, y) => (a < y ? y : a));
-  return { f: f, df: df, d2f: d2f, start: start, end: end, min: min, max: max };
+  return { f, df, d2f, from, to, min, max };
 }
 
-function quadraticCurveLeftSeg(p1: Point, p2: Point, p3: Point): Curve {
+function quadraticCurveLeftSegment(p1: Point, p2: Point, p3: Point): Curve {
   const a =
     ((p1.y - p2.y) * (p1.x - p3.x) - (p1.y - p3.y) * (p1.x - p2.x)) /
     ((p1.x - p2.x) * (p1.x - p3.x) * (p2.x - p3.x));
@@ -20,20 +20,20 @@ function quadraticCurveLeftSeg(p1: Point, p2: Point, p3: Point): Curve {
   const c = p1.y - a * p1.x ** 2 - b * p1.x;
   const f = (x: number): number => a * x ** 2 + b * x + c;
   const df = (x: number): number => 2 * a * x + b;
-  const d2f = (x: number): number => 0 * x + 2 * a;
-  const start = Math.min(p1.x, p2.x, p3.x);
-  const end = [p1.x, p2.x, p3.x].sort()[1];
-  const exts = [f(start), f(end)];
+  const d2f = (): number => 2 * a;
+  const from = Math.min(p1.x, p2.x, p3.x);
+  const to = [p1.x, p2.x, p3.x].sort()[1];
+  const exts = [f(from), f(to)];
   if (a !== 0) {
     const edge = -b / (2 * a);
-    if (start < edge && edge < end) exts.push(f(edge));
+    if (from < edge && edge < to) exts.push(f(edge));
   }
   const min = exts.reduce((a, y) => (a > y ? y : a));
   const max = exts.reduce((a, y) => (a < y ? y : a));
-  return { f: f, df: df, d2f: d2f, start: start, end: end, min: min, max: max };
+  return { f, df, d2f, from, to, min, max };
 }
 
-function quadraticCurveRightSeg(p1: Point, p2: Point, p3: Point): Curve {
+function quadraticCurveRightSegment(p1: Point, p2: Point, p3: Point): Curve {
   const a =
     ((p1.y - p2.y) * (p1.x - p3.x) - (p1.y - p3.y) * (p1.x - p2.x)) /
     ((p1.x - p2.x) * (p1.x - p3.x) * (p2.x - p3.x));
@@ -41,20 +41,20 @@ function quadraticCurveRightSeg(p1: Point, p2: Point, p3: Point): Curve {
   const c = p1.y - a * p1.x ** 2 - b * p1.x;
   const f = (x: number): number => a * x ** 2 + b * x + c;
   const df = (x: number): number => 2 * a * x + b;
-  const d2f = (x: number): number => 0 * x + 2 * a;
-  const start = [p1.x, p2.x, p3.x].sort()[1];
-  const end = Math.max(p1.x, p2.x, p3.x);
-  const exts = [f(start), f(end)];
+  const d2f = (): number => 2 * a;
+  const from = [p1.x, p2.x, p3.x].sort()[1];
+  const to = Math.max(p1.x, p2.x, p3.x);
+  const exts = [f(from), f(to)];
   if (a !== 0) {
     const edge = -b / (2 * a);
-    if (start < edge && edge < end) exts.push(f(edge));
+    if (from < edge && edge < to) exts.push(f(edge));
   }
   const min = exts.reduce((a, y) => (a > y ? y : a));
   const max = exts.reduce((a, y) => (a < y ? y : a));
-  return { f: f, df: df, d2f: d2f, start: start, end: end, min: min, max: max };
+  return { f, df, d2f, from, to, min, max };
 }
 
-function cubicCurveSeg(p1: Point, p2: Point, p3: Point, p4: Point): Curve {
+function cubicCurveSegment(p1: Point, p2: Point, p3: Point, p4: Point): Curve {
   const m1 = p1.y / ((p1.x - p2.x) * (p1.x - p3.x) * (p1.x - p4.x));
   const m2 = p2.y / ((p2.x - p1.x) * (p2.x - p3.x) * (p2.x - p4.x));
   const m3 = p3.y / ((p3.x - p1.x) * (p3.x - p2.x) * (p3.x - p4.x));
@@ -78,75 +78,79 @@ function cubicCurveSeg(p1: Point, p2: Point, p3: Point, p4: Point): Curve {
   const f = (x: number): number => a * x ** 3 + b * x ** 2 + c * x + d;
   const df = (x: number): number => 3 * a * x ** 2 + 2 * b * x + c;
   const d2f = (x: number): number => 6 * a * x + 2 * b;
-  const sort = [p1.x, p2.x, p3.x, p4.x].sort();
-  const start = sort[1];
-  const end = sort[2];
-  const hasExt = b ** 2 - 3 * a * c > 0;
-  const exts = [f(start), f(end)];
-  if (hasExt) {
+  const sorted = [p1.x, p2.x, p3.x, p4.x].sort();
+  const from = sorted[1];
+  const to = sorted[2];
+  const exts = [f(from), f(to)];
+  if (b ** 2 - 3 * a * c > 0) {
     if (a !== 0) {
       const edge1 = (-2 * b + Math.sqrt((2 * b) ** 2 - 12 * a * c)) / (6 * a);
       const edge2 = (-2 * b - Math.sqrt((2 * b) ** 2 - 12 * a * c)) / (6 * a);
-      if (start < edge1 && edge1 < end) exts.push(f(edge1));
-      if (start < edge2 && edge2 < end) exts.push(f(edge2));
+      if (from < edge1 && edge1 < to) exts.push(f(edge1));
+      if (from < edge2 && edge2 < to) exts.push(f(edge2));
     } else if (b !== 0) {
       const edge = -c / (2 * b);
-      if (start < edge && edge < end) exts.push(f(edge));
+      if (from < edge && edge < to) exts.push(f(edge));
     }
   }
   const min = exts.reduce((a, y) => (a > y ? y : a));
   const max = exts.reduce((a, y) => (a < y ? y : a));
-  return { f: f, df: df, d2f: d2f, start: start, end: end, min: min, max: max };
+  return { f, df, d2f, from, to, min, max };
 }
 
 function makeGraph(points: Point[]): Graph {
-  points = points.sort((a, b) => a.x - b.x);
-  const start = points.length > 0 ? points[0].x : null;
-  const end = points.length > 0 ? points[points.length - 1].x : null;
+  //points = points.sort((a, b) => a.x - b.x);
+  const from = points.length > 0 ? points[0].x : null;
+  const to = points.length > 0 ? points[points.length - 1].x : null;
   const curves: Curve[] = [];
   for (let i = 0; i + 1 < points.length; i++) {
     if (i - 1 >= 0) {
       if (i + 2 < points.length) {
         curves.push(
-          cubicCurveSeg(points[i - 1], points[i], points[i + 1], points[i + 2])
+          cubicCurveSegment(
+            points[i - 1],
+            points[i],
+            points[i + 1],
+            points[i + 2]
+          )
         );
       } else {
         curves.push(
-          quadraticCurveRightSeg(points[i - 1], points[i], points[i + 1])
+          quadraticCurveRightSegment(points[i - 1], points[i], points[i + 1])
         );
       }
     } else {
       if (i + 2 < points.length) {
         curves.push(
-          quadraticCurveLeftSeg(points[i], points[i + 1], points[i + 2])
+          quadraticCurveLeftSegment(points[i], points[i + 1], points[i + 2])
         );
       } else {
-        curves.push(linearLineSeg(points[i], points[i + 1]));
+        curves.push(linearLineSegment(points[i], points[i + 1]));
       }
     }
   }
   const f = (x: number): number | null => {
-    if (start === null || x < start) return null;
-    if (end === null || x > end) return null;
+    if (from === null || x < from) return null;
+    if (to === null || x > to) return null;
     const i = points.findIndex(p => p.x > x) - 1;
     if (i >= 0) return curves[i].f(x);
-    if (x === end) return curves[curves.length - 1].f(x);
+    if (x === to) return curves[curves.length - 1].f(x);
     return null;
   };
   const df = (x: number): number | null => {
-    if (start === null || x < start) return null;
-    if (end === null || x > end) return null;
+    if (from === null || x < from) return null;
+    if (to === null || x > to) return null;
     const i = points.findIndex(p => p.x > x) - 1;
     if (i >= 0) return curves[i].df(x);
-    if (x === end) return curves[curves.length - 1].df(x);
+    if (x === to) return curves[curves.length - 1].df(x);
     return null;
   };
   const d2f = (x: number): number | null => {
-    if (start === null || x < start) return null;
-    if (end === null || x > end) return null;
+    if (from === null || x < from) return null;
+    if (to === null || x > to) return null;
     const i = points.findIndex(p => p.x > x) - 1;
     if (i >= 0) return curves[i].d2f(x);
-    if (x === end) return curves[curves.length - 1].d2f(x);
+    if (x === to) return curves[curves.length - 1].d2f(x);
     return null;
   };
   const min =
@@ -157,16 +161,7 @@ function makeGraph(points: Point[]): Graph {
     curves.length > 0
       ? curves.reduce((a, c) => (a.max < c.max ? c : a)).max
       : null;
-  return {
-    f: f,
-    df: df,
-    d2f: d2f,
-    start: start,
-    end: end,
-    min: min,
-    max: max,
-    points: points
-  };
+  return { f, df, d2f, from, to, min, max, points };
 }
 
 function newNote(currentNotes: NoteState[]): NoteState {
