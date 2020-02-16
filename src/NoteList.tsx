107596e7fc8814e73f8d1c491d7d3517da7e5ace
ahuglajbclajep <ahuglajbclajep@gmail.com>
2020-02-16 22:05:44 +0900

    fix NoteList.tsx

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -15,27 +15,38 @@ const NoteList: React.FunctionComponent<Props> = props => {
     () => props.setNotes(prev => [...prev, newNote(prev)]),
     [props]
   );
-  // x が若い順にメモをソートするイベントハンドラ
-  const order = React.useCallback(() => {
+
+  // x が小さい順にメモをソートするイベントハンドラ
+  // グラフに紐づいていないメモは動かさない
+  const ascendingOrder = React.useCallback(() => {
     props.setNotes(prev => {
       const list = prev.map(n => ({ x: n.x, note: n }));
-      let aboveSample: number | null = null;
+
+      // グラフに紐づいていないメモの x を最新の x に合わせる
+      // 例えば list の x が null, 5, 1, null, null, 3, null であった場合、null, 5, 1, 1, 1, 3, 3 のようにする
+      let latestX: number | null = null;
       for (let i = 0; i < list.length; i++) {
-        if (list[i].x === null) list[i].x = aboveSample;
-        else aboveSample = list[i].x;
+        if (list[i].x === null) {
+          list[i].x = latestX;
+        } else {
+          latestX = list[i].x;
+        }
       }
+
       return list
         .sort((a, b) => {
-          // null は int よりも若い
+          // ソートの詳細なアルゴリズムは不明なため、宣言的かつ網羅的に定義する
+          // 先頭のグラフに紐づいていないメモは動かさない
           if (a.x === null && b.x === null) return 0;
           else if (a.x === null) return -1;
           else if (b.x === null) return 1;
-          // int 同士は通常の比較
+          // 数値同士は昇順に並べ、グラフに紐づいていないメモは動かさない
           else return a.x - b.x;
         })
         .map(e => e.note);
     });
   }, [props]);
+
   const notes = props.notes.map(n => {
     // x が定義域内なら f(x), f'(x) を求める
     let y: number | null = null;
@@ -49,6 +60,7 @@ const NoteList: React.FunctionComponent<Props> = props => {
       y = props.graph.f(n.x);
       dy = props.graph.df(n.x);
     }
+
     return (
       <Note
         key={n.id}
@@ -62,13 +74,14 @@ const NoteList: React.FunctionComponent<Props> = props => {
       />
     );
   });
+
   return (
     <div className="note-list">
-      <div onClick={order}>Order By Time</div>
+      <button onClick={ascendingOrder}>Order By Time</button>
       {notes}
-      <div className="new" onClick={add}>
+      <button className="add" onClick={add}>
         + Add
-      </div>
+      </button>
     </div>
   );
 };
