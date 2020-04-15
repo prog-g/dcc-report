632c4fbf32bd5ebbd1c796fa1342228a0e3f1219
curegit <37978051+curegit@users.noreply.github.com>
2020-04-15 20:46:35 +0900

    Fix

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,9 @@ import App from "./App";
 import "./style.css";
 
 ReactDOM.render(<App />, document.getElementById("app"));
-window.onbeforeunload = (): boolean => true;
+
+// ページを閉じようとしたときにアラートを出す
+// null や undefind に評価される値ではアラートが出ないので true をセットする
+window.addEventListener("beforeunload", (e): void => {
+  e.returnValue = true;
+});
