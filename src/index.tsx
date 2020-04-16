42a1974425fa47f3175cd2a5ec8107e0faca9a57
curegit <37978051+curegit@users.noreply.github.com>
2020-04-16 10:41:53 +0900

    ページを閉じようとしたときにアラートを出す (#37)

--- src/index.tsx
+++ src/index.tsx
@@ -4,3 +4,10 @@ import App from "./App";
 import "./style.css";
 
 ReactDOM.render(<App />, document.getElementById("app"));
+
+// ページを閉じようとしたときに確認プロンプトを表示する
+// see https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event#Examples
+window.addEventListener("beforeunload", (e) => {
+  e.preventDefault();
+  e.returnValue = "";
+});
