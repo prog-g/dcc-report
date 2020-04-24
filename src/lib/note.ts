2564714193adda57eb28ed35e4bee3cfb2caa557
ahuglajbclajep <ahuglajbclajep@gmail.com>
2020-04-25 00:34:44 +0900

    fix

--- src/lib/note.ts
+++ src/lib/note.ts
@@ -2,7 +2,7 @@
 // 引数なしで呼ばれた場合や notes が [] の場合は初期値を返す
 function newNote(notes: Notes = []): Note {
   if (!notes.length) return { id: 1, x: null };
-  const newId = Math.max(...notes.map(n => n.id)) + 1;
+  const newId = Math.max(...notes.map((n) => n.id)) + 1;
   return { id: newId, x: null };
 }
 
@@ -17,21 +17,21 @@ function noteColor(id: number): string {
     "green",
     "aqua",
     "blue",
-    "purple"
+    "purple",
   ];
 
   // newNote() の実装より id が 0 以下になることはない
-  return colors[(id % colors.length) - 1];
+  return colors[(id - 1) % colors.length];
 }
 
 // 識別子が id なメモと紐づいたグラフ上の点の番号を求める関数
 // 番号は 1 から与えられ、メモが点と紐づいていなかった場合 null が返る
 function pointNumber(notes: Notes, id: Note["id"]): number | null {
   const i = notes
-    .filter(n => n.x !== null)
+    .filter((n) => n.x !== null)
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     .sort((a, b) => a.x! - b.x!)
-    .findIndex(n => n.id === id);
+    .findIndex((n) => n.id === id);
   return 0 <= i ? i + 1 : null;
 }
 
