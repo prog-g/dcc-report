d30bdcd7bd2edb7e2b1b2493aff0f1f5f5737b5a
ta921 <kodera0925@outlook.jp>
2020-10-27 22:54:44 +0900

    メモ欄を一行に

--- src/Note.tsx
+++ src/Note.tsx
@@ -19,7 +19,7 @@ const Note: React.FunctionComponent<Props> = (props) => {
   React.useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const textarea = ref.current!;
-    textarea.style.height = "88px"; // 26 * 3 +(1 + 4)* 2 = 88 でこれより低くはならない
+    textarea.style.height = "36px"; // 26 * 1 + (1 + 4) * 2 = 36 でこれより低くはならない
     textarea.style.height = `${textarea.scrollHeight}px`;
   }, [content]);
 
